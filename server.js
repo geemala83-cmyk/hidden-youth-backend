2dd7bb7ad64498b5b955d28fa5b25ffaed0bd129
geemala83-cmyk <geemala83@gmail.com>
2026-08-29 10:22:00 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const { Pool } = require("pg");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 /* ==============================
    MIDDLEWARE
