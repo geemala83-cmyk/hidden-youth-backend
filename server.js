c50aa53bb9d557f48a2a454884cdc9288d064f51
geemala83-cmyk <geemala83@gmail.com>
2026-09-09 00:21:39 -0700

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.json({ limit: "2mb" }));
+app.use(express.json({ limit: "8mb" }));
 
 /* =====================================================
    DATABASE
